d997a6f899af197d420c6178b45b6dd88ed778d2
alexRausch <alex@hallundrausch.de>
2016-12-05 12:02:49 +0100

    Add files via upload

---
+++ scripts/userData.js
@@ -0,0 +1,11 @@
+// this data will be loaded when there are no settings saved on localStorage.
+var userData = {
+	customerNumber: 	        "",
+	customerLogin: 		        "",
+	customerSignature: 	        "",
+	interfacePartnerNumber: 	"",
+	interfacePartnerSignature:	"",
+	targetMarket:				"DE",
+	language:					"de_DE",
+	additionalHosts:			[]
+};
